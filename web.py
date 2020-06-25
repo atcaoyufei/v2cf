a07f4b5ac3df3f12d887a51a52f0756163c09ce7
atcaoyufei <atcaoyufei@gmail.com>
2020-06-25 13:03:06 +0800

    init

--- web.py
+++ web.py
@@ -1,14 +1,9 @@
 import os
 
-from flask import Flask, jsonify
 
-app = Flask(__name__, static_url_path='')
-
-
-@app.route('/')
-def root():
-    return jsonify(os.environ)
+def main():
+    print(os.environ)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    main()
