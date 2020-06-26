bdbe721f5a188182312ecd4a084f659bf6b1a540
atcaoyufei <atcaoyufei@gmail.com>
2020-06-26 20:32:32 +0800

    v2

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+SH_PATH=/tmp/cf-app
+install -d $SH_PATH
+cd $SH_PATH
+
+read -p "请输入你的应用名称：" IBM_APP_NAME
+echo "应用名称：${IBM_APP_NAME}"
+read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
+if [ -z "${IBM_MEM_SIZE}" ]; then
+  IBM_MEM_SIZE=256
+fi
+echo "内存大小：${IBM_MEM_SIZE}"
+
+cat >${SH_PATH}/main.py <<EOF
+import os
+
+
+def main():
+    print(os.environ)
+
+
+if __name__ == '__main__':
+    main()
+
+EOF
+
+cat >${SH_PATH}/Procfile <<EOF
+web: /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+EOF
+
+cat >${SH_PATH}/manifest.yml <<EOF
+applications:
+- path: .
+  name: ${IBM_APP_NAME}
+  random-route: true
+  memory: ${IBM_MEM_SIZE}M
+  buildpacks:
+    - https://github.com/cloudfoundry/python-buildpack.git
+EOF
+
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v4.25.1/v2ray-linux-64.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+rm -rf /tmp/v2ray
+
+install -d /usr/local/etc/v2ray
+UUID=$(python -c 'import uuid; print uuid.uuid1()')
+cat <<EOF >/usr/local/etc/v2ray/config.json
+{
+    "log":{
+        "loglevel":"warning"
+    },
+    "inbounds": [
+        {
+            "port": 8080,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "alterId": 64
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+ibmcloud target --cf
+ibmcloud cf install
+ibmcloud cf push
+
+exit 0
